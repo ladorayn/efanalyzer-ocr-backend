319c6654f47bafac4820102b32d653f4f9474d35
Lado Najib <ladonajib@Lados-MacBook-Pro.local>
2026-05-18 00:27:48 +0700

    fix

--- main.py
+++ main.py
@@ -167,5 +167,5 @@ async def recognize(file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host='0.0.0.0', port=port)
